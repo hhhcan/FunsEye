0151006c09e0a01d1b1235622b0febbab3b53589
can <1406018192@qq.com>
2022-06-27 10:44:39 +0800

    自定义Application，进行全局的初始化操作

---
+++ app/src/main/java/com/funs/eye/FunsEyeApplication.kt
@@ -0,0 +1,29 @@
+package com.funs.eye
+
+import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+
+/**
+ * 自定义Application，进行全局的初始化操作。
+ *
+ * @author can
+ */
+class FunsEyeApplication : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+        context = this;
+    }
+
+    companion object {
+        lateinit var context: Context
+    }
+
+    //Android 5.0 以上默认内置分包处理，可忽略不写
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
