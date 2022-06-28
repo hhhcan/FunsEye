9dbdf74d26203ae1bfcc35c8a1c3f6937d552416
can <1406018192@qq.com>
2022-06-28 10:50:50 +0800

    两秒内连按两次退出应用

--- app/src/main/java/com/funs/eye/ui/MainActivity.kt
+++ app/src/main/java/com/funs/eye/ui/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.funs.eye.ui
 
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import com.funs.eye.R
 import com.funs.eye.databinding.ActivityMainBinding
+import com.funs.eye.extension.showToast
 import com.funs.eye.ui.common.ui.BaseActivity
+import com.funs.eye.util.GlobalUtil
 
 class MainActivity : BaseActivity() {
 
@@ -11,6 +15,9 @@ class MainActivity : BaseActivity() {
     val binding: ActivityMainBinding
         get() = _binding!!
 
+    private var backPressTime = 0L
+
+    private val fragmentManager: FragmentManager by lazy { supportFragmentManager }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +27,27 @@ class MainActivity : BaseActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        _binding =null
+        _binding = null
     }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStack()
+        } else {
+            processBackPressed()
+        }
+    }
+
+    private fun processBackPressed() {
+        val now = System.currentTimeMillis()
+        if (now - backPressTime > 2000) {
+            String.format(GlobalUtil.getString(R.string.press_again_to_exit), GlobalUtil.appName)
+                .showToast()
+            backPressTime = now
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
 }
