460f488816cd55b051530bff6b0c75fc2c848d69
can <1406018192@qq.com>
2022-06-28 16:11:08 +0800

    SmartRefreshLayout:下拉刷新、上拉加载&自定义Footer

--- app/src/main/java/com/funs/eye/FunsEyeApplication.kt
+++ app/src/main/java/com/funs/eye/FunsEyeApplication.kt
@@ -4,6 +4,10 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
+import com.funs.eye.ui.common.view.NoStatusFooter
+import com.funs.eye.util.GlobalUtil
+import com.scwang.smart.refresh.header.MaterialHeader
+import com.scwang.smart.refresh.layout.SmartRefreshLayout
 
 /**
  * 自定义Application，进行全局的初始化操作。
@@ -12,6 +16,34 @@ import androidx.multidex.MultiDex
  */
 class FunsEyeApplication : Application() {
 
+
+    init {
+        SmartRefreshLayout.setDefaultRefreshInitializer { context, layout ->
+            layout.setEnableLoadMore(true)
+            layout.setEnableLoadMoreWhenContentNotFull(true)
+        }
+
+        SmartRefreshLayout.setDefaultRefreshHeaderCreator { context, layout ->
+            layout.setEnableHeaderTranslationContent(true)
+            MaterialHeader(context).setColorSchemeResources(
+                R.color.black,
+                R.color.black,
+                R.color.black
+            )
+        }
+
+        SmartRefreshLayout.setDefaultRefreshFooterCreator { context, layout ->
+            layout.setEnableFooterFollowWhenNoMoreData(true)
+            layout.setEnableFooterTranslationContent(true)
+            layout.setFooterHeight(153f)
+            layout.setFooterTriggerRate(0.6f)
+            NoStatusFooter.REFRESH_FOOTER_NOTHING = GlobalUtil.getString(R.string.footer_not_more)
+            NoStatusFooter(context).apply {
+                setAccentColorId(R.color.colorTextPrimary)
+                setTextTitleSize(16f)
+            }
+        }
+    }
 
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/com/funs/eye/ui/community/follow/FollowFragment.kt
+++ app/src/main/java/com/funs/eye/ui/community/follow/FollowFragment.kt
@@ -1,8 +1,28 @@
 package com.funs.eye.ui.community.follow
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.funs.eye.databinding.FragmentRefreshLayoutBinding
 import com.funs.eye.ui.common.ui.BaseFragment
 
 class FollowFragment : BaseFragment() {
+
+    private var _binding: FragmentRefreshLayoutBinding? = null
+
+    private val binding
+        get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRefreshLayoutBinding.inflate(layoutInflater, container, false)
+        return super.onCreateView(binding.root)
+    }
 
     companion object {
         fun newInstance() = FollowFragment()
