b0f989a90360400bc9cbbb84feb12418e0cea46c
can <1406018192@qq.com>
2022-06-28 09:43:14 +0800

    Add MainActivity and Study how to use ConstraintLayout/Group

--- app/src/main/java/com/funs/eye/ui/MainActivity.kt
+++ app/src/main/java/com/funs/eye/ui/MainActivity.kt
@@ -1,7 +1,25 @@
 package com.funs.eye.ui
 
+import android.os.Bundle
+import com.funs.eye.databinding.ActivityMainBinding
 import com.funs.eye.ui.common.ui.BaseActivity
 
 class MainActivity : BaseActivity() {
 
+    var _binding: ActivityMainBinding? = null
+
+    val binding: ActivityMainBinding
+        get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding =null
+    }
 }
